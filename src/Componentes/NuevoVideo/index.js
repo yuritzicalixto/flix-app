3c0b0299f2a5fbd16a6947458b27513b1897d84b
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-07-08 19:00:59 -0600

    1/2 Changes

--- src/Componentes/NuevoVideo/index.js
+++ src/Componentes/NuevoVideo/index.js
@@ -1,17 +1,28 @@
 import "./NuevoVideo.css"
 
 function NuevoVideo(){
-    return <section className="form-main">
-        <h1>NUEVO VIDEO</h1>
-        <p>Complete el formulario para crear una nueva tarjeta de video</p>
-        <hr/>
-        <h2>Crear Tarjeta</h2>
-        <hr/>
-        <form className="form-container">
-            <label className="campo-etiqueta"></label>
-            <input className="campo-completo" required/>
-        </form>
-    </section>
+    return <section className=" form form-main">
+    <form  className="form-container">
+        <div className="form-container-title">
+            <h1>NUEVO VIDEO</h1>
+            <p>COMPLETE EL FORMULARIO PARA CREAR UNA NUEVA TARJETA DE VIDEO</p>
+        </div>
+        <div className="form-container-head">
+            <h2>Crear Tarjeta</h2>
+        </div>
+        <div className="formulario">
+        <CampoTexto titulo="Nombre" placeholder="Ingresa el titulo" required/>
+        <ListaOpciones/>
+        {/* <CampoTexto titulo="Categoría" placeholder="seleccione una categoría" required/> */}
+        <CampoTexto titulo="Imagen" placeholder="ingrese un enlace de imagen" required/>
+        <CampoTexto titulo="Video" placeholder="ingrese el enlace del video" required/>
+        <CampoTexto titulo="Descripción" placeholder="¿De qué se trata este vídeo?" required/>
+        <Boton>
+            Crear
+        </Boton>
+        </div>
+    </form>
+</section>
 }
 
 export default NuevoVideo
